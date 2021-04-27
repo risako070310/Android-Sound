80b57e1add70bb705f4423ee2f7ba25842f8ef3d
risako070310 <risako070310@gmail.com>
2021-04-27 16:52:39 +0900

    done second mission

--- app/src/main/java/com/risako070310/sound/MainActivity.kt
+++ app/src/main/java/com/risako070310/sound/MainActivity.kt
@@ -2,6 +2,7 @@ package com.risako070310.sound
 
 import android.media.MediaPlayer
 import android.os.Bundle
+import android.view.MotionEvent
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
 
@@ -12,9 +13,19 @@ class MainActivity : AppCompatActivity() {
 
         val drumSound = MediaPlayer.create(this, R.raw.drum_sound)
 
-        drumImage.setOnClickListener{
-            drumSound.seekTo(0)
-            drumSound.start()
+        drumImage.setOnTouchListener { view, event ->
+            if(event.action == MotionEvent.ACTION_DOWN){
+                drumImage.setImageResource(R.drawable.drum_playing_image)
+
+                drumSound.seekTo(0)
+                drumSound.start()
+
+            } else if (event.action == MotionEvent.ACTION_UP){
+                drumImage.setImageResource(R.drawable.drum_image)
+
+            }
+
+            true
         }
     }
 }
