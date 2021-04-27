7ae8409c425acbd286b9ac87f9cf5c18d72467a0
risako070310 <risako070310@gmail.com>
2021-04-27 18:17:27 +0900

    done challenge mission

--- app/src/main/java/com/risako070310/sound/MainActivity.kt
+++ app/src/main/java/com/risako070310/sound/MainActivity.kt
@@ -12,6 +12,9 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         val drumSound = MediaPlayer.create(this, R.raw.drum_sound)
+        val pianoSound = MediaPlayer.create(this, R.raw.piano_sound)
+        val cymbalSound = MediaPlayer.create(this, R.raw.cymbal_sound)
+        val guitarSound = MediaPlayer.create(this, R.raw.guitar_sound)
 
         drumImage.setOnTouchListener { view, event ->
             if(event.action == MotionEvent.ACTION_DOWN){
@@ -27,5 +30,52 @@ class MainActivity : AppCompatActivity() {
 
             true
         }
+
+        pianoImage.setOnTouchListener { view, event ->
+            if(event.action == MotionEvent.ACTION_DOWN){
+                pianoImage.setImageResource(R.drawable.piano_playing_image)
+
+                pianoSound.seekTo(0)
+                pianoSound.start()
+
+            } else if (event.action == MotionEvent.ACTION_UP){
+                pianoImage.setImageResource(R.drawable.piano_image)
+
+            }
+
+            true
+        }
+
+        cymbalImage.setOnTouchListener { view, event ->
+            if(event.action == MotionEvent.ACTION_DOWN){
+                cymbalImage.setImageResource(R.drawable.cymbal_playing_image)
+
+                cymbalSound.seekTo(0)
+                cymbalSound.start()
+
+            } else if (event.action == MotionEvent.ACTION_UP){
+                cymbalImage.setImageResource(R.drawable.cymbal_image)
+
+            }
+
+            true
+        }
+
+        guitarImage.setOnTouchListener { view, event ->
+            if(event.action == MotionEvent.ACTION_DOWN){
+                guitarImage.setImageResource(R.drawable.guitar_playing_image)
+
+                guitarSound.seekTo(0)
+                guitarSound.start()
+
+            } else if (event.action == MotionEvent.ACTION_UP){
+                guitarImage.setImageResource(R.drawable.guitar_image)
+
+            }
+
+            true
+        }
+
+
     }
 }
