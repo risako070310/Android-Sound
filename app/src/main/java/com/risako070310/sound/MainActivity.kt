32823bdd0baf031571ca5129a2d55868d7858363
risako070310 <risako070310@gmail.com>
2021-04-27 16:22:32 +0900

    first commit

---
+++ app/src/main/java/com/risako070310/sound/MainActivity.kt
@@ -0,0 +1,20 @@
+package com.risako070310.sound
+
+import android.media.MediaPlayer
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val drumSound = MediaPlayer.create(this, R.raw.drum_sound)
+
+        drumImage.setOnClickListener{
+            drumSound.seekTo(0)
+            drumSound.start()
+        }
+    }
+}
